dd5706244113a0de73caa6425054dc66eb8ae0ad
Vinícius Leite <viniciusleitealves@gmail.com>
2024-03-19 00:06:01 -0300

    Criação da Página de login

--- Front-end/src/App.jsx
+++ Front-end/src/App.jsx
@@ -1,12 +1,23 @@
 import { useState } from 'react'
 import './App.css'
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import {HomePage} from "./pages/HomePage/HomePage"
+import {LoginPage} from "./pages/LoginPage/LoginPage"
+
 
 function App() {
 
   return (
-    <>
-     <h1>Hello world!</h1>
-    </>
+    <div className='App'>
+      <Router>
+        <Routes>
+          <Route path='/' element={<HomePage/>}/>
+          <Route path='/login' element={<LoginPage/>}/>
+          <Route path='*' element={<h1>Page not Found</h1>}></Route>
+        </Routes>
+      </Router>
+
+    </div>
   )
 }
 
---
+++ Front-end/src/pages/LoginPage/LoginPage.jsx
@@ -0,0 +1,38 @@
+import "./LoginPage.css"
+import { Link } from "react-router-dom";
+
+export const LoginPage = ()=>{
+    return(
+        <div className="main">
+            <div className="box-login">
+                <h1>Login</h1>
+
+                <form action="" className="login-form">
+                    <div className="input-perso">
+                        <input type="text" placeholder="Username"/>
+                        <i>icon</i>
+                    </div>
+
+                    <div className="input-perso">
+                        <input type="password" placeholder="Password"/>
+                        <i>icon</i>
+                    </div>
+
+                    <div className="aux">
+                        <label htmlFor="">
+                            <input type="checkbox"/> Remember me
+                        </label>
+
+                        <Link to="/" className="link-reset">Forgot Password</Link>
+                    </div>
+
+                    <div className="btn-box">
+                        <button className="login">Login</button>
+                    </div>
+
+                    <h3>Don't have an account? <Link to="/" className="link-register link-reset">Register</Link></h3>
+                </form>
+            </div>
+        </div>
+    )
+}
